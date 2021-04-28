2c45de644a415ed8b51b3c740f5facd6b7f7acd9
Kunal Bhatti <kunal.bhatti15@gmail.com>
2021-04-28 12:24:43 +0530

    improved routing for handling empty and invalid paths

--- src/app/app-routing.moduler.ts
+++ src/app/app-routing.moduler.ts
@@ -5,15 +5,24 @@ import {
   RouterModule,
   Routes
 } from "@angular/router";
+import {
+  ProjectsComponent
+} from "./projects/projects.component";
 
 const routes: Routes = [{
-    path: 'projects',
-    loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)
-  }, {
-    path: 'about-me',
-    loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule)
-  }
-]
+  path: '',
+  pathMatch: 'full',
+  redirectTo: '/projects'
+}, {
+  path: 'projects',
+  loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule)
+}, {
+  path: 'about-me',
+  loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule)
+}, {
+  path: '**',
+  component: ProjectsComponent
+}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
